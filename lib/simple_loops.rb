74d92cf7c9e9cbf3f0be25640e752a9df57ba1b1
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-12 21:44:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,3 +24,12 @@ def output_array(array)
     counter +=1
   end
 end
+
+def return_string_array(array)
+  counter= 0
+
+  while counter< array.length do
+    puts array.to_s
+    counter += 1
+  end
+end
