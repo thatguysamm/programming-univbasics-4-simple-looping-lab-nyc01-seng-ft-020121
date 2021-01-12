cc5b109106c79f8618116e207bd0feba3cce0f90
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-12 21:48:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,9 +27,11 @@ end
 
 def return_string_array(array)
   counter= 0
+  new_array= []
 
   while counter< array.length do
-    puts array.to_s
+    new_array.push(array[counter].to_s)
     counter += 1
   end
+  new_array
 end
